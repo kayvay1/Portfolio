68691dff359d9ff3eeb94850f6bedc599d2dc03e
Anil Sharma <127938838+kayvay1@users.noreply.github.com>
2024-09-08 20:02:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const hamburger_menu = document.querySelector('.hamburger'),
+    container = document.querySelector('.container');
+    let logo = document.querySelectorAll(".logo");
+    let menu = document.querySelectorAll(".menu");
+    let bar = document.querySelectorAll(".bar");
+   
+
+
+
+
+    
+    hamburger_menu.addEventListener("click", function(){
+        container.classList.toggle("active");
+        
+    } )
+      
+      
+    
+
+
+
+
+ 
